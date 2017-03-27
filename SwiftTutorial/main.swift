bfdb83f518919be2ef33110fae413fbdacca5ee9
achilmaza <angie.chilmaza@gmail.com>
2017-03-27 18:40:28 -0400

    Version1

--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -10,3 +10,11 @@ import Foundation
 
 print("Hello, World!")
 
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
